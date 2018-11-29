e0925d4ec86657c4a014bc31c5565f900c778433
yanjy <1019975412@qq.com>
2018-11-29 22:50:30 +0800

    增加堆栈

--- devC_project/main.c
+++ devC_project/main.c
@@ -1,14 +1,28 @@
+/*堆栈练习题*/
+/*在不传递参数的情况下，在print函数中打印出main函数中数组的元素*/
 #include <stdio.h>
-#include <string.h>
+
+void print()
+{
+	unsigned int _ebp;
+	int i;
+	__asm(
+		"mov _ebp,ebp"
+	);
+	int *p = (int *)(*(int *)_ebp-4-4-4-7*4);
+	for (i = 0; i<7; i++)
+	{
+		printf("%d\t", p[i]);
+	}
+	return;
+}
 int main(int argc, char const *argv[])
 {
-	unsigned char a[1000];
-	int i;
-	printf("%d\n",strlen(a));
-	for ( i= 0; i < 1000; ++i)
-	{
-		a[i] = -1-i;
-	}
-	printf("%d\n", strlen(a));
+	int a = 1;
+	int b = 2;
+	char c = 'c';
+	int arr[] = {11,12,13,14,15,16,17};
+
+	print();
 	return 0;
 }
