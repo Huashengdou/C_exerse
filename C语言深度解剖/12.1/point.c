247e5db2cefe92572338f261efc45f7fbc1e17cd
yanjy <1019975412@qq.com>
2018-12-01 16:39:36 +0800

    增加数组指针&&指针数组

---
+++ C语言深度解剖/12.1/point.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int arr[4][4] = {
+						1,2,3,4,
+						5,6,7,8,
+						9,10,11,12,
+						13,14,15,16
+					};
+	/*数组指针*/
+	int (*p1)[4];
+	/*指针数组*/
+	int *p2[4];
+	int i,j,k;
+	/*将二维数组的地址赋给指向四个元素的数组指针*/
+	p1 = arr;
+	printf("使用数组指针的方式访问二维数组arr\n");
+	
+	for (i=0; i<4; i++)
+	{
+		for (j=0; j<4; j++)
+		{
+			printf("arr[%d][%d] = %d\t\n", i, j, *(*(p1+i)+j));
+		}
+		printf("\n");
+	}
+	return 0;
+}
