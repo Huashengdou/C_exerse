7257d6a8e425af3ef637f18b37ec19702cb0d918
yanjy <1019975412@qq.com>
2018-12-07 23:00:26 +0800

    数组定义

--- C语言深度解剖/exerse2.c
+++ C语言深度解剖/exerse2.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+int n = 9;
 int main(int argc, char const *argv[])
 {
 	char a[1000] = "hello";
 	int i;
+	int b[n];
+	n = 10;
+
+	printf("b[9] = %d\n", b[11]);
 	printf("%d\n",strlen(a));
 	for ( i = 0; i < 1000; ++i)
 	{
