d911d37575190830ac783efc18c218efb20bd7fc
ubuntu_yan <1019975412@qq.com>
2019-03-31 06:46:14 -0700

    add观察点

--- linux_c/breakpoint/main.c
+++ linux_c/breakpoint/main.c
@@ -6,11 +6,11 @@ int main(void)
 	char input[5];
 
 	while(1){
+		sum = 0;
 		scanf("%s",input);
 		for(i=0; input[i]!='\0'; i++)
 			sum = sum*10 + input[i] - '0';
 		printf("input=%d\n",sum);
-		//sum = 0;
 	}
 	return 0;
 }
