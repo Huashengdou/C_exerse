7fa0dddb842642418d856ca41afd8f32ce2990e2
ubuntu_yan <1019975412@qq.com>
2019-04-02 08:17:12 -0700

    add makefile

---
+++ linux_c/makefile/add_num.c
@@ -0,0 +1,4 @@
+int add_num(int i, int j)
+{
+	return i + j;
+}
---
+++ linux_c/makefile/main.c
@@ -0,0 +1,10 @@
+#include <stido.h>
+#include <add_num.h>
+
+int main(void)
+{
+	int i = 1, j = 2;
+	int a = add_num(i,j);
+	pirntf("result = %d\n",a);
+	return 0;
+}
