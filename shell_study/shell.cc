d997a8d7b6f968a8c91578d4bf7ab04d6bb18d8f
yanjy <1019975412@qq.com>
2019-05-26 11:26:35 +0800

    add my first c++ code

---
+++ shell_study/shell.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	cout << "The program name is " << argv[0] << "." << endl;
+	if (argc > 1)
+	{
+		cout << "With " << argc -1 << " args as follows:" << endl;
+		for (int i = 0; i < argc; ++i)
+		{
+			cout << argv[i] << endl;
+		}
+	}
+	else
+	{
+		cout << "With " << argc - 1 << " arguments" << endl;
+	}
+	return 0;
+}
